e00ce51fda2726e22aee13a0cc5504e0cd9c51e9
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-09-13 13:15:10 -0400

    Minor changes to the text

--- partials/citedpubs.php
+++ partials/citedpubs.php
@@ -8,7 +8,7 @@
         It is critical for bioinformaticians to have their work cited, Some of the publications which used our Gateway and cited us. </p>
          <br></br>
         <h4 class="text-left">Happy to be part of it!!!</h4>
-        
+        <br></br>
         <ul style="list-style-type:square">
 
         <li style="font-size:18px" align="left">Benler, Sean, Ana Georgina Cobián-Güemes, Katelyn McNair, Shr-Hau Hung, Kyle Levi, Rob Edwards, and Forest Rohwer. "A diversity-generating retroelement encoded by a globally ubiquitous Bacteroides phage." Microbiome 6, no. 1 (2018): 191.</li>
