1af94b6af99df89b0f0f2fd1e2fccfeb832dcf51
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-09-13 13:07:56 -0400

    Adding content to Cited page

---
+++ partials/citedpubs.php
@@ -0,0 +1,23 @@
+<div class="container">
+  <div class="content">
+    <div class="text-center">
+        <h2 class="text-center">Search SRA Supported Publications</h2>
+        <hr class="separation-line blue-bg"/>
+
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        It is critical for bioinformaticians to have their work cited, Some of the publications which used our Gateway and cited us.
+        Thank you! Happy to be part of it!!!
+        Please be sure to not only cite searchsra.org, but to also cite the tools that we have used to build this amazing resource.
+        </p>
+
+        <ul style="list-style-type:square">
+
+        <li style="font-size:18px" align="left">Benler, Sean, Ana Georgina Cobián-Güemes, Katelyn McNair, Shr-Hau Hung, Kyle Levi, Rob Edwards, and Forest Rohwer. "A diversity-generating retroelement encoded by a globally ubiquitous Bacteroides phage." Microbiome 6, no. 1 (2018): 191.</li>
+        <li style="font-size:18px" align="left">Wandro, Stephen Joseph. "Evolution of bacteriophages infecting Enterococcus from the human microbiome." PhD diss., UC Irvine, 2019.</li>
+        <li style="font-size:18px" align="left">Hagbø, Mari, Anuradha Ravi, Inga Leena Angell, Marianne Sunde, Jane Ludvigsen, Dzung B. Diep, Steven L. Foley et al. "Experimental support for multidrug resistance transfer potential in the preterm infant gut microbiota." Pediatric research (2019).</li>
+
+        </ul>
+        <br></br>
+    </div>
+  </div>
+</div>
